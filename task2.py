7beeaf856c7228f3d1e77e05925c14b2d155a9c2
Snehal Oturkar <snehaloturkar18@gmail.com>
2026-01-13 10:44:54 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,9 @@
+# Take first name and last name as input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenate first name and last name to form full name
+full_name = first_name + " " + last_name
+
+# Print a personalized greeting
+print("Hello,", full_name + "! Welcome to Python programming.")
